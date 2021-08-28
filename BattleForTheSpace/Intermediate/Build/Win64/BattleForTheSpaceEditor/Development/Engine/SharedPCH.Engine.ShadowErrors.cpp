ea7acdaa9622b609fb098599f4101251c9453f8a
PolygonBear <Semenyaka.maxim@yandex.ru>
2021-08-28 10:15:48 +0300

    Delete
    Delete project

--- BattleForTheSpace/Intermediate/Build/Win64/BattleForTheSpaceEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
+++
@@ -1,2 +0,0 @@
-// Compiler: 14.29.30133
-#include "D:/Private/MyProjects/PrivateProjects/MyDepot/UE4/BattleForTheSpace/BattleForTheSpace/Intermediate/Build/Win64/BattleForTheSpaceEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
